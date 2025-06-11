a655764416760a7f43907f05b32658dff8540c05
Ansh Chikara <chikara.ansh15@gmail.com>
2025-06-11 11:27:49 +0530

    ordering

--- 3)ConditionalStatements&Loops.py
+++ 3)ConditionalStatements&Loops.py
@@ -29,8 +29,18 @@ match command:
 # Loops 
 
 for i in range(5):
-    print(i)  
+    print(i) 
     
+for g in "hello":
+    print(g)
+
+for item in [10, 20, 30]:
+    print(item)
+
+for key, value in {"a": 1, "b": 2}.items():
+    print(key, value)  #tuple unpacking , will understand in later programs
+    
+# while loop
 x = 100
 while x < 105:
     print(x)
@@ -65,5 +75,28 @@ while True:
         break
     else :
       print("enter again")
-      
+  
+     
+#nested loop      
+              
+for i in range(3):
+    for j in range(2):
+        print(f"i={i}, j={j}")
+ 
+#for else loop        
+                     
+for i in range(5):
+    if i == 10:
+        break
+    print(i)
+else:
+    print("Completed loop without break")
+    
+# using pass statements
+for i in range(5):
+    pass  # I will write this later
+    
+squares = [x**2 for x in range(5)]
+print(squares)
+ 
  
